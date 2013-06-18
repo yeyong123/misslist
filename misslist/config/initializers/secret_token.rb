85cd8838046781c83f61e4e8cc2f7d118f8b1b05
kevin <kevin@kevin.(none)>
2013-06-18 23:22:13 +0800

    初始CSS模板

---
+++ misslist/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Misslist::Application.config.secret_token = '1d4325d5b705523ec803125d4c008d127a8fea76bac2bc639bc85f38f2400d57aeecaf5ed04562b7d4aa46a60ac3ce3d10b604b3e33ade41e01f9769a80ae8b3'
